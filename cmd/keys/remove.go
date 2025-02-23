e7aba1b58fe7cd027da71b26e704c2e7c0730ce8
Khash Sajadi <khash@sajadi.co.uk>
2025-02-23 15:38:15 -0800

    ask for confirmation before removing keys

--- cmd/keys/remove.go
+++ cmd/keys/remove.go
@@ -23,33 +23,41 @@ This will download the current JWKS, remove the specified key, and upload the up
 
 func init() {
 	Cmd.AddCommand(removeCmd)
+
+	removeCmd.Flags().BoolP("yes", "y", false, "Skip confirmation prompt")
 }
 
 func runRemove(_ *cobra.Command, args []string) error {
 	ctx := context.Background()
 	keyID := args[0]
 
-	client, err := getAdminClient(ctx)
-	if err != nil {
-		return err
+	yes, _ := removeCmd.Flags().GetBool("yes")
+
+	if yes || utils.ExpectYes("Are you sure you want to remove key?. This operation is not reversible.") {
+		client, err := getAdminClient(ctx)
+		if err != nil {
+			return err
+		}
+
+		resp, err := client.RemoveKey(ctx, &proto.RemoveKeyRequest{
+			KeyId: keyID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to remove key: %w", err)
+		}
+
+		out := utils.OutData{
+			Headers: table.Row{"KeyId", "Algorithm", "Created"},
+			Rows:    []table.Row{},
+		}
+
+		for _, key := range resp.Keys {
+			out.Rows = append(out.Rows, table.Row{key.KeyId, key.Algorithm, time.Unix(key.CreatedAt, 0).Format(time.RFC3339)})
+			out.RawData = append(out.RawData, key)
+		}
+
+		return utils.Print(out)
 	}
 
-	resp, err := client.RemoveKey(ctx, &proto.RemoveKeyRequest{
-		KeyId: keyID,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to remove key: %w", err)
-	}
-
-	out := utils.OutData{
-		Headers: table.Row{"KeyId", "Algorithm", "Created"},
-		Rows:    []table.Row{},
-	}
-
-	for _, key := range resp.Keys {
-		out.Rows = append(out.Rows, table.Row{key.KeyId, key.Algorithm, time.Unix(key.CreatedAt, 0).Format(time.RFC3339)})
-		out.RawData = append(out.RawData, key)
-	}
-
-	return utils.Print(out)
+	return nil
 }
